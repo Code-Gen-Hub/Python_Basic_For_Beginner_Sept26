c8aa3f27772700df8f8d0649e507184b7fcd96ac
CodeGen <161267485+Code-Gen-Hub@users.noreply.github.com>
2026-09-14 21:06:30 +0800

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,26 @@
+# This is a comment!
+
+# 1. variables
+# variable name = value / data
+name = 'Connie'
+food = 'nasi lemak'
+# - number cannot place at front
+# - only can use _ to variable name
+drink_1 = 'teh c'
+my_drink = 'kopi'
+
+# 2. data types - 4
+# a) string - quotation mark ""
+introduction = "I am Connie"
+# b) integer - always whole nmumber
+age = 20
+# c) float - always with decimal place
+price = 19.99
+# d) boolean - only have True / False
+is_student = True
+
+# 3. input / output
+print(age)
+print(introduction)
+username = input('Please enter your username? ')
+print(username)
