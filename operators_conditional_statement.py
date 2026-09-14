a02e9e78010a25df27ad994f7009458fbd4e7b39
CodeGen <161267485+Code-Gen-Hub@users.noreply.github.com>
2026-09-14 21:07:13 +0800

    Create operators_conditional_statement.py

---
+++ operators_conditional_statement.py
@@ -0,0 +1,25 @@
+# 4.1 arithmetic operators
+print(10 + 11) # addition
+print(10 - 5) # subtraction
+print(5 * 2) # multiplication
+print(9 / 3) # division (float result)
+print(9 // 3) # floor division (integer result)
+
+# 4.2 comparison operators
+print(10 == 11) # equal to
+print(10 != 11) # not equal to
+print(10 > 11) # greater than
+print(10 >= 11)
+print(11 < 12) # less than
+print(11 <= 11)
+
+# 5. conditional statement
+is_raining = False
+is_cloudy = False
+
+if is_raining: # if True:
+  print('bring an umbrella!')
+elif is_cloudy:
+  print('wear a jacket!')
+else:
+  print('wear a dress!')
